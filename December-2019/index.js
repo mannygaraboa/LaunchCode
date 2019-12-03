a0ee1c209b20bae1f621204e47cabfb58a2b9664
Manuel Garaboa <mannygaraboa@gmail.com>
2019-12-03 13:42:18 -0500

    First Commit + Coding Assessment Solution

---
+++ December-2019/index.js
@@ -0,0 +1,37 @@
+// Jeremy Anderson 12:26 PM
+
+// Write a function that returns the position or positions of the lowest valued integer and can handle these arrays or any other array 
+
+// No High Order functions
+// Only Length and push()
+
+let figures1 = [-1, -7, 1, 5, -7, 0];
+let figures2 = [4,5,6];  
+
+function lowValue (array)
+{
+	lowNum = array[0];
+	let lowPos = [];
+	for(let i = 0; i < array.length; i++)
+	{
+		if(array[i] > array[i+1] && lowNum > array[i+1])
+		{
+			lowNum = array[i+1];
+      console.log("New Low Number: " + lowNum);
+		}
+		else if(array[i] < array[i+1] && lowNum > array[i])
+		{
+			lowNum = array[i];
+      console.log("New Low Number: " + lowNum);
+		}
+
+		if(array[i] === lowNum)
+		{
+      console.log(array[i] + " : " + lowNum);
+      console.log("Pushing Position: " + i);
+			lowPos.push(i);
+		}
+	}
+  return lowPos;
+}
+lowValue(figures2);
